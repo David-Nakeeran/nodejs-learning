2b97d14b70c1b046bd9a8164f46f8d78e83cf4d8
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-21 16:48:40 +0000

    formatting

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+// const { generateRandomNumber, celciusToFahrenheit } = require("./utils.js");
+// console.log(`Random number: ${generateRandomNumber()}`);
+// console.log(`Celcius to fahrenhiet: ${celciusToFahrenheit(0)}`);
+import getPosts, { getPostLength } from "./postController.js";
+console.log(getPosts());
+console.log(`Posts lenght: ${getPostLength()}`);
