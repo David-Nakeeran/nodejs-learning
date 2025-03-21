1d1b429be130223b3c5e6522d10212c8e6574468
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2025-03-21 16:49:26 +0000

    Testing out exports

---
+++ postController.js
@@ -0,0 +1,9 @@
+const posts = [
+  { id: 1, title: "Post One" },
+  { id: 1, title: "Post One" },
+];
+
+// export const getPosts = () => posts;
+const getPosts = () => posts;
+export const getPostLength = () => posts.length;
+export default getPosts;
